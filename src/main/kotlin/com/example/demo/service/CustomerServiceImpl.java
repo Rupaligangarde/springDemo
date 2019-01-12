68c2c3ea4e0c5427c92f01ad590bae3053b78b16
Rupali Gangarde <rupalig@thoughtworks.com>
2019-01-12 17:11:13 +0530

    Spring xml config - Beam using constructor method

--- src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
+++ src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
@@ -8,12 +8,13 @@ import java.util.List;
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
     }
 }
