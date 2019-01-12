62c493112776b7182d697b79653b4a6bdfac1e49
Rupali Gangarde <rupalig@thoughtworks.com>
2019-01-12 17:19:52 +0530

    Spring xml config - Bean using autowire byName method

--- src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
+++ src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
@@ -9,12 +9,16 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
+//    public CustomerServiceImpl(CustomerRepository customerRepository) {
+//        this.customerRepository = customerRepository;
+//    }
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
+    }
+
+    public void setFoo(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
     }
 }
