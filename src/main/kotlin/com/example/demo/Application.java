593fff8b316fa4eeb649468bdddb8ce75a6b9b0f
Rupali Gangarde <rupalig@thoughtworks.com>
2019-01-12 16:45:31 +0530

    Spring xml config - Beam using setter method

--- src/main/kotlin/com/example/demo/Application.java
+++ src/main/kotlin/com/example/demo/Application.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.service.CustomerService;
-import com.example.demo.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        CustomerService customerService = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
+
         System.out.println(customerService.findAll().get(0).getFirstName());
         System.out.println(customerService.findAll().get(0).getLastName());
     }
--- src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
+++ src/main/kotlin/com/example/demo/service/CustomerServiceImpl.java
@@ -2,16 +2,18 @@ package com.example.demo.service;
 
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
-import com.example.demo.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
     }
 
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 }
